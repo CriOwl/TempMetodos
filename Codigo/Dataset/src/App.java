6cd75fa26aed9140a37a360b679e4e16b69ae5db
CriOwl <cristhiancarrillolocopaul@gmail.com>
2025-12-13 17:31:27 -0500

    Python

--- Codigo/Dataset/src/App.java
+++ Codigo/Dataset/src/App.java
@@ -12,15 +12,16 @@ public class App {
             String line = scanner.nextLine();
                 String[] values = line.split(",");
                 writer.write(values[1]+","+values[6]+","+values[19]+","+values[20]+","+values[21]+","+values[26]+","+values[30]+","+values[32]+","+values[38]+","+values[39]+"\n");
-
+                int i=0;
             while (scanner.hasNextLine()) { 
+                i++;
+                System.out.println(i);
                 String line2 = scanner.nextLine();
                 String[] values2= line2.split(",");
                 if (values2[8].contains("3D: XP")) {
                     writer.write(values2[1]+","+values2[6]+","+","+values2[19]+","+values2[20]+","+values2[21]+","+values2[26]+","+values2[30]+","+values2[32]+","+values2[38]+","+values2[39]+"\n");
                 } 
             } 
-            
             writer.close();
         } catch (Exception e) {
             System.out.println(e);
