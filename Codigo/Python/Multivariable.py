19795cae326c6d83b34fc175ed2c51b7ebb7f411
AnndyR <anndy.rengifo@epn.edu.ec>
2025-12-14 17:34:06 -0500

    Ajuste a multivariable en la estimacion

--- Codigo/Python/Multivariable.py
+++ Codigo/Python/Multivariable.py
@@ -1,6 +1,15 @@
 import math
 
 def transponer_matriz(matriz):
+    """
+    Docstring for transponer_matriz
+    Calcular la matriz transpuesta
+
+    :Parametros matriz (list of list): matriz original de dimensiones m x n
+
+    Retorna: Matriz transpuesta de dimensiones n x m
+    """
+
     filas = len(matriz)
     columnas = len(matriz[0])
     transpuesta = [[0] * filas for _ in range(columnas)]
@@ -11,25 +20,17 @@ def transponer_matriz(matriz):
     return transpuesta
 
 def multiplicar_matrices(matriz_a, matriz_b):
-    filas_A = len(matriz_a)
-    cols_A = len(matriz_a[0])
-    filas_B = len(matriz_b)
-    cols_B = len(matriz_b[0])
-
-    if cols_A != filas_B:
-        raise ValueError("Dimensiones incompatibles para multiplicar!!")
+    """
+    Realizamos la multiplicacion entre dos matrices que sean compatibles
     
-    C = [[0] * cols_B for _ in range(filas_A)]
+    :param matriz_a: Matriz A de tamano m x n
+    :param matriz_b: Matriz B de tamano n x p
 
-    for i in range(filas_A):
-        for j in range(cols_B):
-            suma = 0
-            for k in range(cols_A):
-                suma += matriz_a[i][k] * matriz_b[k][j]
-            C[i][j] = suma
-    return C
+    Retorna: Matriz resultante C de tamano m x p
 
-def multiplicar_matrices(matriz_a, matriz_b):
+    Devuelve un error si las dinesiones no permiten la multiplicacion
+    """
+
     filas_A = len(matriz_a)
     cols_A = len(matriz_a[0])
     filas_B = len(matriz_b)
@@ -49,16 +50,31 @@ def multiplicar_matrices(matriz_a, matriz_b):
     return C
 
 def imprimir_matriz(matriz, nombre="Matriz"):
+    """
+    Imprime la matriz
+    """
+
     print(f"{nombre}:")
     for fila in matriz:
         print([round(x, 4) for x in fila])
     print()
 
+
 """
 Sistemas de Gauss-Jordan
 """
-
 def resolver_sistema_gauss(A, B):
+    """
+    Docstring for resolver_sistema_gauss
+    Resuelve un sitema de ecuaciones lienales usando eliminacion de GAUSS
+    El sistema tiene la forma:
+    A * x = B
+
+    Retorna: Vector solucion del sistema
+
+    Devuelve un error si el sistema no tiene solucion unica
+    """
+
     n = len(A)
     M = [A[i][:] + B[i][:] for i in range(n)]
 
@@ -90,10 +106,16 @@ Minimos cuadrados
 """
 
 def ajuste_minimos_cuadrados(datos_indices, target):
-    X = [] 
-    for fila in datos_indices:
-        X.append([1.0] + fila) 
+    """
+    Docstring for ajuste_minimos_cuadrados
+    Calculamos los coeficientes del emodelo lineal mediante minimos cuadrados
 
+    El modelo tiene la forma:
+    y = beta_0 + beta_1 * x_1 + beta_2 * x_2 + ... + beta_n * x_n
+
+    Retorna: Vector de coeficientes del modelo
+    """
+    X = [[1.0] + fila for fila in datos_indices]
     Y = [[val] for val in target]
 
     print("--- Pasos Intermedios ---")
@@ -107,9 +129,51 @@ def ajuste_minimos_cuadrados(datos_indices, target):
 
     return beta
 
-def estimar(indices_entrada, coeficientes):
-    y_estimado = coeficientes[0] 
-    for i in range(len(indices_entrada)):
-        y_estimado += coeficientes[i+1] * indices_entrada[i]
+def estimar(variables, coeficientes):
+    """
+    Docstring for estimar
+    Estimamos el valor de salida para una sola observacion
+
+    :param variables: Valiables independientes
+    :param coeficientes: Coeficientes del modelo
+
+    Retorna: Valor estimado
+    """
+    y_estimado = coeficientes[0]
+    for i in range(len(variables)):
+        y_estimado += coeficientes[i + 1] * variables[i]
     return y_estimado
 
+def estimar_dataset(datos_indices, coeficientes):
+    """
+    Docstring for estimar_dataset
+    Estimamos los valores de salida para multipes observaciones
+
+    :param datos_indices: Matriz de variables indepedientes
+    :param coeficientes: Coeficientes del modelo
+
+    Retorna: lista de valores estimados
+    """
+
+    estimaciones = []
+    for fila in datos_indices:
+        estimaciones.append(estimar(fila, coeficientes))
+    return estimaciones
+
+
+def error_cuadratico_medio(y_real, y_estimado):
+    """
+    Docstring for error_cuadratico_medio
+    Calculamos el error cuadratico Medio del modelo
+
+    :param y_real: Valores reales
+    :param y_estimado: Valores estimados
+
+    Retorna: Error cuadratico medio
+    """
+
+    n = len(y_real)
+    suma = 0.0
+    for i in range(n):
+        suma =+ (y_real[i] - y_estimado[i]) ** 2
+    return suma / n
